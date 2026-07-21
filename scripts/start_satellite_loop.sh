188a7090ea1bc825e05bc1df7115bf0bd6d55cac
rikeffc <rikeffc@gmail.com>
2026-07-21 10:50:20 -0300

    add pulse_server env var to fix ffplay in tmux

--- scripts/start_satellite_loop.sh
+++ scripts/start_satellite_loop.sh
@@ -8,12 +8,15 @@ echo "[1/3] Iniciando servidor de áudio (PulseAudio)..."
 pulseaudio -k 2>/dev/null
 pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --load="module-sles-source" --load="module-sles-sink" --exit-idle-time=-1
 
-echo "[2/3] Entrando na pasta do projeto..."
+echo "[2/3] Configurando variáveis de ambiente..."
+export PULSE_SERVER=127.0.0.1
+
+echo "[3/3] Entrando na pasta do projeto..."
 # Descobre o diretório raiz independente de onde o script foi chamado
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR/.."
 
-echo "[3/3] Iniciando loop de ressurreição..."
+echo "[4/4] Iniciando loop de ressurreição..."
 # Loop infinito: Se a internet cair, o websocket morre, o python fecha.
 # O loop espera 5 segundos e liga o python de novo.
 while true; do
